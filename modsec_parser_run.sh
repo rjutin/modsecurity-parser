ad8fc3b21d3b0d959e848b1f0c5c9112cb629965
rjutin <106729548+rjutin@users.noreply.github.com>
2022-07-25 14:03:21 +0700

    cron bash script
    cron bash script

--- modsec_parser_run.sh
+++ modsec_parser_run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 #runner for everyday generation reports for modsecurity
+#put it in cron
+#sudo crontab -e
+#@daily /home/leqs/modsecurity-parser/modsec_parser_run.sh
 echo $(date) >> /var/log/modsecurity/cron_modsecurity.log
 python3 /home/leqs/modsecurity-parser/modsecurity-parser.py -f /var/log/modsecurity/modsec_audit.log --version3 >> /var/log/modsecurity/cron_modsecurity.log
 fail2ban-client status sshd >> /var/log/modsecurity/cron_modsecurity.log
